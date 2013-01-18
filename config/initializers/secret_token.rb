839d3ceaa210c73c12cfb5741dfa21ce32356ec6
Christian Estrella <personal@christianestrella.com>
2013-01-18 12:49:56 -0600

    Project initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsmeeting::Application.config.secret_token = 'f64bd567e21a4694a0889e4db51cff48806f752f6f22013a4cfe6ce8b3c3e783b9d6baceea00c799469ef72bb70d499d70f1d1e3f70b1ffd360274a6266e48fb'
